240f0c114b8c7931fd60948c5cdaeda13d4a3965
Diana <crystaljerry@gmail.com>
2017-02-02 19:53:42 -0700

    Finish basic requirements and add searchbar

---
+++ codepath-1-movie-app/Pods/Target Support Files/Pods-codepath-1-movie-app/Pods-codepath-1-movie-app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_codepath_1_movie_app : NSObject
+@end
+@implementation PodsDummy_Pods_codepath_1_movie_app
+@end
--- codepath-1-movie-app/codepath-1-movie-app/MoviesViewController.swift
+++ codepath-1-movie-app/codepath-1-movie-app/MoviesViewController.swift
@@ -7,32 +7,70 @@
 //
 
 import UIKit
+import AFNetworking
+import MBProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchControllerDelegate, UISearchResultsUpdating, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var errorNotifView: UIView!
     
     var movies: [NSDictionary]?
     
+    var filteredMovies: [NSDictionary]?
+    
+    var searchController : UISearchController!
+    
+    let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.searchController = UISearchController(searchResultsController:  nil)
+        
+        self.searchController.searchResultsUpdater = self
+        self.searchController.delegate = self
+        self.searchController.searchBar.delegate = self
+        
+        self.searchController.hidesNavigationBarDuringPresentation = false
+        self.searchController.dimsBackgroundDuringPresentation = false
+        
+        
+        
+        self.navigationItem.titleView = searchController.searchBar
+        if let navigationBar = navigationController?.navigationBar {
+            navigationBar.backgroundColor = UIColor.black
+        }
+        
+        errorNotifView.isHidden = true;
+        
         // Do any additional setup after loading the view.
         tableView.dataSource = self
         tableView.delegate = self
         
-        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+//        movieSearchBar.delegate = self
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        MBProgressHUD.showAdded(to: self.view, animated: true)
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
             if let data = data {
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     print(dataDictionary)
+                    MBProgressHUD.hide(for: self.view, animated: true)
                     
                     self.movies = dataDictionary["results"] as? [NSDictionary]
+                    self.filteredMovies = self.movies
                     self.tableView.reloadData()
                 }
+            } else {
+                MBProgressHUD.hide(for: self.view, animated: true)
+                self.errorNotifView.isHidden = false;
             }
         }
         task.resume()
@@ -45,7 +83,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let movies = movies { // if movies is not nill, then assign to constant movies
-            return movies.count
+            return filteredMovies!.count
         } else {
             return 0
         }
@@ -54,15 +92,90 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
         
-        let movie = movies![indexPath.row]
+        let movie = filteredMovies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
         
+        let baseUrl = "http://image.tmdb.org/t/p/w500"
+        let posterPath = movie["poster_path"] as! String
+        
+        let imageUrl = NSURL(string: baseUrl + posterPath)
+        
+        cell.posterView.setImageWith(imageUrl as! URL)
+        
         print("row \(indexPath.row)")
         return cell
+    }
+    
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        
+        // ... Create the URLRequest `myRequest` ...
+        
+        // Configure session so that completion handler is executed on main UI thread
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                self.errorNotifView.isHidden = true;
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    print(dataDictionary)
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    
+                    self.movies = dataDictionary["results"] as? [NSDictionary]
+                    self.filteredMovies = self.movies
+                    self.tableView.reloadData()
+                }
+            } else {
+                MBProgressHUD.hide(for: self.view, animated: true)
+                self.errorNotifView.isHidden = false;
+            }
+        }
+        task.resume()
+        // Reload the tableView now that there is new data
+            self.tableView.reloadData()
+            
+            // Tell the refreshControl to stop spinning
+            refreshControl.endRefreshing()
+    }
+
+    func searchBarTextDidBeginEditing(_ searchBar:  UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+
+    public func updateSearchResults(for searchController: UISearchController) {
+        if let searchText = searchController.searchBar.text {
+            if searchText.isEmpty {
+                filteredMovies = movies
+            }
+            else
+            {
+                filteredMovies = movies!.filter({(dataItem: NSDictionary) -> Bool in
+                    let title = dataItem["title"] as! String
+                    if title.range(of: searchText, options: .caseInsensitive) != nil {
+                        return true
+                    }
+                    else {
+                        return false
+                    }
+                })
+            }
+            tableView.reloadData()
+        }
+        
     }
     /*
     // MARK: - Navigation
@@ -73,5 +186,4 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Pass the selected object to the new view controller.
     }
     */
-
 }
